a3f9ad00cb759fcc74f676237d17a1f4027d1349
pablitooo04 <pablodauga@gmail.com>
2026-02-16 20:13:18 +0100

    better

--- main.py
+++ main.py
@@ -10,17 +10,7 @@ except ImportError:
     print("Error: numpy module not found!")
     exit(1)
 
-
-def auto_loop() -> None:
-    """
-    Simple function to scroll through generations.
-    """
-    try:
-        if auto_mode:
-            I1.compute_next_generation()
-            root.after(1, auto_loop)
-    except KeyboardInterrupt:
-        exit(1)
+from time import time
 
 
 class Interface:
@@ -35,25 +25,24 @@ class Interface:
         self.size: int = size
         self.edit_mode: bool = False
         self.gap: float = self.size/len(self.matrix)
+        self.edit_on_img = tk.PhotoImage(file="assets/edit_on.png")
+        self.edit_off_img = tk.PhotoImage(file="assets/edit_off.png")
+        self.play_img = tk.PhotoImage(file="assets/play.png")
+        self.stop_img = tk.PhotoImage(file="assets/stop.png")
+        self.auto_mode = False
+        self.gen_per_second = 999999
+        self.last_frame_time = time()
+        self.fps = 0
+        self.colors = {
+            0: "#FFFFFF",
+            1: "#000000",
+        }
+        self.canvas = tk.Canvas(width=window_size, height=window_size)
+        self.canvas.configure(bg=self.colors[0])
 
-        for i in range(len(self.matrix) + 1):
-            pos = int(i * self.gap)
-            canvas.create_line(pos, 0, pos, self.size, fill="#C7C6C6")
-            canvas.create_line(0, pos, self.size, pos, fill="#C7C6C6")
+        self.draw_grid()
 
-        for n_ligne in range(len(self.matrix)):
-            for n_case in range(len(self.matrix)):
-                if self.matrix[n_ligne][n_case] != 0:
-                    canvas.create_rectangle(
-                        int(n_case * self.gap),
-                        int(n_ligne * self.gap),
-                        int((n_case + 1) * self.gap),
-                        int((n_ligne + 1) * self.gap),
-                        fill=dico_couleurs[self.matrix[n_ligne][n_case]],
-                        outline="#C7C6C6"
-                    )
-
-        canvas.place(x=0, y=0)
+        self.canvas.place(x=0, y=0)
 
     def get_neighbors(self, cell: tuple[int, int]) -> int:
         """Return the neighbors count of a cell.
@@ -97,41 +86,62 @@ class Interface:
             (grid == 1) & (neighbors == 2))).astype(int)
 
         self.matrix = new_grid.tolist()
-        self.draw_grid(self.matrix)
+        self.draw_grid()
 
-    def draw_grid(self, current_matrix: list[list[int]]) -> None:
+    def draw_grid(self) -> None:
         """Redraw the grid on the canvas based on the given matrix."""
-        canvas.delete("all")
+        self.canvas.delete("all")
         for i in range(len(self.matrix) + 1):
             pos = int(i * self.gap)
-            canvas.create_line(pos, 0, pos, self.size, fill="#C7C6C6")
-            canvas.create_line(0, pos, self.size, pos, fill="#C7C6C6")
+            self.canvas.create_line(pos, 0, pos, self.size, fill="#C6C6C6")
+            self.canvas.create_line(0, pos, self.size, pos, fill="#C6C6C6")
 
-        self.new_matrix = current_matrix
+        self.new_matrix = self.matrix
         for n_line in range(len(self.new_matrix)):
             for n_col in range(len(self.new_matrix)):
                 if self.new_matrix[n_line][n_col] != 0:
-                    canvas.create_rectangle(
+                    self.canvas.create_rectangle(
                         int(n_col * self.gap),
                         int(n_line * self.gap),
                         int((n_col + 1) * self.gap),
                         int((n_line + 1) * self.gap),
-                        fill=dico_couleurs[self.new_matrix[n_line][n_col]],
+                        fill=self.colors[self.new_matrix[n_line][n_col]],
                         outline="#C6C6C6"
                     )
 
-    def toggle_edit(self) -> None:
+        if self.edit_mode:
+            self.canvas.create_image(
+                15, 15, image=self.edit_on_img, anchor="nw")
+        else:
+            self.canvas.create_image(
+                15, 15, image=self.edit_off_img, anchor="nw")
+
+        if self.auto_mode:
+            self.canvas.create_image(
+                15 + 128 + 15, 15, image=self.play_img, anchor="nw")
+        else:
+            self.canvas.create_image(
+                15 + 128 + 15, 15, image=self.stop_img, anchor="nw")
+
+    def toggle_edit(self, event=None) -> None:
         """Toggle edit mode and update the canvas background.
 
         Edit mode can only be changed when automatic mode is disabled.
         """
-        if not auto_mode:
-            if not I1.edit_mode:
+        if not self.auto_mode:
+            if not self.edit_mode:
                 self.edit_mode = True
-                canvas.configure(bg="#7799d1")
+                self.canvas.configure(bg="#7799d1")
             else:
                 self.edit_mode = False
-                canvas.configure(bg="#f6f6f6")
+                self.canvas.configure(bg=self.colors[0])
+
+        if self.edit_mode:
+            self.canvas.create_image(
+                15, 15, image=self.edit_on_img, anchor="nw")
+        else:
+            self.canvas.create_image(
+                15, 15, image=self.edit_off_img, anchor="nw")
 
     def set_random_grid(self):
         """
@@ -142,59 +152,77 @@ class Interface:
 
         self.matrix = np.random.randint(
             0, 2, (len(self.matrix), len(self.matrix)))
-        self.draw_grid(self.matrix)
+        self.draw_grid()
 
+    def toggle_auto_mode(self, event=None):
+        """
+        Toggle automatic simulation mode handling spacebar press.
 
-def on_click(event: tk.Event) -> None:
-    """
-    Handle a left-click on the grid.
+        Automatic mode can only be enabled when edit mode is disabled.
+        """
+        self.auto_mode = not self.auto_mode
+        if self.auto_mode:
+            self.canvas.create_image(
+                15 + 128 + 15, 15, image=self.play_img, anchor="nw")
+            self.auto_loop()
+        else:
+            self.canvas.create_image(
+                15 + 128 + 15, 15, image=self.stop_img, anchor="nw")
 
-    If edit mode is disabled, compute the next generation.
-    If edit mode is enabled, activate the clicked cell.
-    """
-    if not auto_mode:
-        if not I1.edit_mode:
-            I1.compute_next_generation()
+    def auto_loop(self):
+        if not self.auto_mode:
+            return
+
+        # Calcul du temps écoulé
+        now = time()
+        dt = now - self.last_frame_time
+        self.last_frame_time = now
+
+        # Calcul FPS (évite division par zéro)
+        if dt > 0:
+            self.fps = 1 / dt
+
+        # Affichage dans le titre
+        root.title(f"Conway’s Game of Life — {self.fps:.2f} FPS")
+
+        # Next frame
+        self.compute_next_generation()
+        root.after(
+            int(1000/self.gen_per_second) +
+            int(not bool(int(1000/self.gen_per_second))),
+            self.auto_loop
+        )
+
+    def on_click(self, event: tk.Event) -> None:
+        """
+        Handle a left-click on the grid.
+
+        If edit mode is disabled, compute the next generation.
+        If edit mode is enabled, activate the clicked cell.
+        """
+
+        if not self.edit_mode:
+            self.compute_next_generation()
         else:
             cell = (
-                min(int(event.x / I1.gap), len(I1.matrix[0]) - 1),
-                min(int(event.y / I1.gap), len(I1.matrix) - 1),
+                min(int(event.x / self.gap), len(self.matrix[0]) - 1),
+                min(int(event.y / self.gap), len(self.matrix) - 1),
             )
-            I1.matrix[cell[1]][cell[0]] = 1
+            self.matrix[cell[1]][cell[0]] = 1
 
-            canvas.create_rectangle(
+            self.canvas.create_rectangle(
                 int(cell[0]*I1.gap),
                 int(cell[1]*I1.gap),
                 int((cell[0]+1)*I1.gap),
                 int((cell[1]+1)*I1.gap),
-                fill=dico_couleurs[1]
+                fill=self.colors[1]
             )
-
-
-def toggle_auto_mode(event: tk.Event):
-    """
-    Toggle automatic simulation mode handling spacebar press.
-
-    Automatic mode can only be enabled when edit mode is disabled.
-    """
-    global auto_mode
-    if not I1.edit_mode:
-        auto_mode = not auto_mode
-        if auto_mode:
-            auto_loop()
 
 
 if __name__ == "__main__":
     # === Proprities ===
 
-    dico_couleurs = {
-        0: "#FFFFFF",
-        1: "#000000",
-    }
-
-    auto_mode = False
-
-    matrix_size = 250
+    matrix_size = 500
 
     window_size = 1000
 
@@ -208,18 +236,16 @@ if __name__ == "__main__":
     icon = tk.PhotoImage(file="assets/icon.png")
     root.iconphoto(True, icon)
 
-    canvas = tk.Canvas(width=window_size, height=window_size)
-
     matrix = [[0 for _ in range(matrix_size)] for _ in range(matrix_size)]
 
     I1 = Interface(matrix, window_size)
 
     # === Binds ===
 
-    root.bind("<space>", toggle_auto_mode)
-    canvas.bind("<Button-1>", on_click)
-    canvas.bind("<Button-3>", lambda event: I1.toggle_edit())
+    root.bind("<space>", I1.toggle_auto_mode)
     root.bind("r", lambda event: I1.set_random_grid())
+    I1.canvas.bind("<Button-1>", I1.on_click)
+    I1.canvas.bind("<Button-3>", I1.toggle_edit)
 
     # === Run ===
 
